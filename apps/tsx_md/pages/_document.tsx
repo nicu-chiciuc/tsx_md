bcece5e09f85997a68acfaf2d12ec603251745f0
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-03 17:05:34 +0200

    move shadcn up

--- apps/tsx_md/pages/_document.tsx
+++ apps/tsx_md/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import { cn } from '../src/shadcn/lib/utils'
+import { cn } from '../src/lib/utils'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={cn('min-h-screen bg-background font-sans antialiased')}>
+      <body className={cn('bg-background min-h-screen font-sans antialiased')}>
         <Main />
         <NextScript />
       </body>
