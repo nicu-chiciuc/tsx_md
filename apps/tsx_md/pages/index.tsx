63b1bda2036b4e76ae655cf4e9fe3ed2321a11bf
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-11 01:05:05 +0200

    don't render on server text

--- apps/tsx_md/pages/index.tsx
+++ apps/tsx_md/pages/index.tsx
@@ -1,8 +1,15 @@
 import { GithubIcon } from '@/my_components/githubCorner/githubForkIcon'
 import { MainNavigationMenu } from '@/my_components/mainNavMenu'
+import { useEffect, useState } from 'react'
 import Ztext from 'react-ztext'
 
 const IndexPage: React.FC = () => {
+  const [showText, setShowText] = useState(false)
+
+  useEffect(() => {
+    setShowText(true)
+  }, [])
+
   return (
     <div className="flex flex-col items-center">
       <GithubIcon />
@@ -10,21 +17,23 @@ const IndexPage: React.FC = () => {
       <MainNavigationMenu />
 
       <div className="flex h-screen items-center justify-center">
-        <Ztext
-          depth="1rem"
-          direction="both"
-          event="pointer"
-          eventRotation="30deg"
-          eventDirection="default"
-          fade={false}
-          layers={10}
-          perspective="400px"
-          style={{
-            fontSize: '12rem',
-          }}
-        >
-          tsx.md
-        </Ztext>
+        {showText && (
+          <Ztext
+            depth="1rem"
+            direction="both"
+            event="pointer"
+            eventRotation="30deg"
+            eventDirection="default"
+            fade={false}
+            layers={10}
+            perspective="400px"
+            style={{
+              fontSize: '12rem',
+            }}
+          >
+            tsx.md
+          </Ztext>
+        )}
       </div>
     </div>
   )
