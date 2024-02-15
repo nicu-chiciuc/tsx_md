41b4cc46308a2f33edf1adf3c70c6798ee3a8cd8
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-15 23:33:17 +0200

    default shadcn

--- apps/validate_form/pages/_app.tsx
+++ apps/validate_form/pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from 'next/font/google'
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+}
+  return (
+    /**
+     * https://github.com/vercel/next.js/issues/44840#issuecomment-1442347859
+     *
+     * I wanted to add the font from shadcn https://ui.shadcn.com/docs/installation/next
+     */
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        :root {
+          --font-sans: ${fontSans.style.fontFamily};
+        }`,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
