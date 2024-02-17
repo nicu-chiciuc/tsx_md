2bb612fc2648beb4b429569065c36899c218099e
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-17 18:05:01 +0200

    put link to tsx.md

--- apps/gif_md/pages/index.tsx
+++ apps/gif_md/pages/index.tsx
@@ -6,7 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={`flex h-full items-center justify-center bg-black ${inter.className}`}>
-      <img src="/dancing_baby.gif" />
+      <a href="https://tsx.md">
+        <img src="/dancing_baby.gif" />
+      </a>
     </main>
   )
 }
