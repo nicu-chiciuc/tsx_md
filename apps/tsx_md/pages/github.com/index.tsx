4e378ff3a5223b4b093b225b77b8f9d6d5071786
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-02-27 00:30:13 +0200

    finish index

--- apps/tsx_md/pages/github.com/index.tsx
+++ apps/tsx_md/pages/github.com/index.tsx
@@ -3,6 +3,9 @@ import { MainNavigationMenu } from '@/my_components/mainNavMenu'
 import { GithubCorner } from '@/my_components/githubCorner/githubForkIcon'
 import Link from 'next/link'
 import { getArticles } from '@/server/blogServing'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import { MarkdownComponentsMonaco } from '@/my_components/monacoEditor'
 
 export default function Page(props: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
@@ -12,12 +15,7 @@ export default function Page(props: InferGetStaticPropsType<typeof getStaticProp
 
       <div className="flex items-center justify-center ">
         <article className="prose lg:prose-xl">
-          <h2>Typescript markdown viewer</h2>
-
-          <p>
-            If you're viewing some markdown file on github that contains Typescript: for example
-            [https://github.com/robolex-app/public_ts/blob/main/README.md]
-          </p>
+          <MDXRemote {...props.mdxSerialized} components={MarkdownComponentsMonaco} />
         </article>
       </div>
     </div>
@@ -25,7 +23,36 @@ export default function Page(props: InferGetStaticPropsType<typeof getStaticProp
 }
 
 export const getStaticProps = async () => {
+  const mdContent = `
+## Typescript markdown viewer
+
+If you're viewing some markdown file on github that contains Typescript, e.g.
+
+[github.com/robolex-app/public_ts/blob/main/README.md](https://github.com/robolex-app/public_ts/blob/main/README.md)
+
+you can add 'tsx.md' before the github and render the markdown with full Typescript support.
+
+For example, the above link becomes:
+
+[tsx.md/github.com/robolex-app/public_ts/blob/main/README.md](https://tsx.md/github.com/robolex-app/public_ts/blob/main/README.md)
+
+  `
+
+  const mdxSerialized = await serialize(
+    mdContent,
+
+    {
+      parseFrontmatter: true,
+
+      mdxOptions: {
+        format: 'md',
+      },
+    }
+  )
+
   return {
-    props: {},
+    props: {
+      mdxSerialized,
+    },
   }
 }
